e9c8b564dbf896fbd1ced4663f89dc497abd2ab1
lizexiong <373664968@qq.com>
2018-01-15 13:56:38 +0800

    加入redux相关配置

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import createHistory from 'history/createBrowserHistory';
+import { Route, Switch } from 'react-router-dom';
+import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const history = createHistory();
+const middleware = routerMiddleware(history);
+
+const store = createStore(
+  combineReducers({
+    router: routerReducer,
+  }),
+  applyMiddleware(thunk, middleware),
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route exact path="/" component={App} />
+      </Switch>
+    </ConnectedRouter>
+  </Provider>
+, document.getElementById('root'));
 registerServiceWorker();
