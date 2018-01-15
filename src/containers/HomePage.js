357887bf3f72815e0d59ba92a54b64a4207c8235
lizexiong <373664968@qq.com>
2018-01-15 18:01:54 +0800

    update

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -4,12 +4,12 @@ import logo from '../static/images/demo.jpg';
 class HomePage extends Component {
   render() {
     return (
-      <div className="HomePage">
-        <header className="HomePage-header">
-          <img src={logo} className="HomePage-logo" alt="logo" />
-          <h1 className="HomePage-title">Welcome to React</h1>
+      <div className="homepage">
+        <header>
+          <img src={logo} alt="logo" />
+          <h1>Welcome to React</h1>
         </header>
-        <p className="HomePage-intro">
+        <p>
           To get started, edit <code>src/HomePage.js</code> and save to reload.
         </p>
       </div>
