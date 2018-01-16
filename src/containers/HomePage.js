0a5c87dca3109470f6801bc5f2e26eab661d5042
lizexiong <373664968@qq.com>
2018-01-16 15:18:03 +0800

    引入grid和图片轮播

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,64 +1,84 @@
 import React, { Component } from 'react';
-import banner from '../static/images/banner.jpg';
+import { Carousel, Grid } from 'antd-mobile';
 
 class HomePage extends Component {
   constructor(props, context) {
     super(props, context);
+    this.onIndexPageInit = this.onIndexPageInit.bind(this);
     this.state = {
       bannerHeight: 'auto',
-      sectionHeight: 'auto'
+      contentHeight: 'auto',
+      bannerData: ['1', '2', '3'],
     };
   }
   componentWillMount() {
-    console.log(document.body.clientWidth)
-    const bWidth = document.body.clientWidth;
-    const bHeight = bWidth / 2.5;
-    const vHeight = document.documentElement.clientHeight;
-    const sHeight = (vHeight - bHeight) / 3;
+    this.onIndexPageInit();
+  }
+  componentDidMount() {
+    window.addEventListener('resize', this.onIndexPageInit);
     this.setState({
-      bannerHeight: bHeight,
-      sectionHeight: sHeight,
+      bannerData: ['IJOtIlfsYdTyaDTRVrLI', 'AiyWuByWklrrUDlFignR', 'TekJlZRVCjLFexlOCuWn'],
     });
   }
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onIndexPageInit);
+  }
+  onIndexPageInit() {
+    const documentWidth = document.documentElement.clientWidth;
+    const documentHeight = document.documentElement.clientHeight;
+    const bannerHeight = documentWidth / 2.5;
+    const contentHeight = documentHeight - bannerHeight;
+    this.setState({
+      bannerHeight,
+      contentHeight,
+    })
+  }
   render() {
-    console.log(this.state)
-    const { bannerHeight, sectionHeight } = this.state;
-    const bannerStyle = {
-      height: bannerHeight + 'px'
+    const { bannerHeight, contentHeight, bannerData } = this.state;
+    const contentStyle = {
+      height: contentHeight + 'px'
     };
-    const sectionStyle = {
-      height: sectionHeight + 'px'
+    const itemStyle = {
+      height: (contentHeight / 3) + 'px'
     }
+    const itemData = Array.from(new Array(6)).map((_val, i) => ({
+      icon: 'https://gw.alipayobjects.com/zos/rmsportal/nywPmnTAvTmLusPxHPSu.png',
+      text: `name${i}`,
+    }));
     return (
       <div className="homepage">
-        <header style={bannerStyle}>
-          <img src={banner} alt="banner" />
+        <header>
+          <Carousel
+            autoplay={true}
+            autoplayInterval={5000}
+            infinite
+            selectedIndex={0}
+          >
+            {bannerData.map(val => (
+              <a
+                key={val}
+                style={{ display: 'inline-block', width: '100%', height: bannerHeight + 'px' }}
+              >
+                <img
+                  src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
+                  alt=""
+                  style={{ width: '100%', verticalAlign: 'top' }}
+                  onLoad={() => {
+                    // fire window resize event to change height
+                    window.dispatchEvent(new Event('resize'));
+                    this.setState({ imgHeight: 'auto' });
+                  }}
+                />
+              </a>
+            ))}
+          </Carousel>
         </header>
-        <div className="index-content clearfix">
-          <section style={sectionStyle}>
-            <i className="iconfont icon-shouyinzhongxin"></i>
-            <p>我的</p>
-          </section>
-          <section style={sectionStyle}>
-            <i className="iconfont icon-shouyinzhongxin"></i>
-            <p>我的</p>
-          </section>
-          <section style={sectionStyle}>
-            <i className="iconfont icon-shouyinzhongxin"></i>
-            <p>我的</p>
-          </section>
-          <section>
-            <i className="iconfont icon-shouyinzhongxin"></i>
-            <p>我的</p>
-          </section>
-          <section>
-            <i className="iconfont icon-shouyinzhongxin"></i>
-            <p>我的</p>
-          </section>
-          <section>
-            <i className="iconfont icon-shouyinzhongxin"></i>
-            <p>我的</p>
-          </section>
+        <div className="index-content" style={contentStyle}>
+          <Grid
+            data={itemData}
+            columnNum={2}
+            itemStyle={itemStyle}
+          />
         </div>
       </div>
     );
